9602d30c9b084b92700bdfbae16f4859f11311c2
BielDiaz <biel.diaz@gmail.com>
2020-11-05 12:54:12 +0100

    Implemented new button with hover effect

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,13 +1,41 @@
-import React from 'react'
-import { HeroContainer, HeroBg, VideoBg } from './HeroElements'
+import React, { useState } from 'react'
 import Video from '../../videos/video.mp4'
+import { Button } from '../ButtonElement'
+import {
+    HeroContainer,
+    HeroBg,
+    VideoBg,
+    HeroContent,
+    Heroh1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight,
+} from './HeroElements'
+
+
 const HeroSection = () => {
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover);
+    }
+
     return (
         <HeroContainer id="home">
             <HeroBg>
                 <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
             </HeroBg>
+            <HeroContent>
+                <Heroh1>Crypto Virtual Company </Heroh1>
+                <HeroP>Sign up for a new account</HeroP>
+                <HeroBtnWrapper>
+                    <Button onMouseEnter={onHover} onMouseLeave={onHover} to="signup" primary="true" dark="true"> Get Started! {hover ? <ArrowForward /> : <ArrowRight />} </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
+
         </HeroContainer>
+
     )
 }
 
