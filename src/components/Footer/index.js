85313df591cdc0d93ba3a864db0058065f5093ac
BielDiaz <biel.diaz@gmail.com>
2020-11-09 11:31:54 +0100

    Footer Section implemented

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,22 @@
 import React from 'react'
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinksWrapper,
+    FooterLinkTitle,
+    FooterLink,
+    FooterLinkItems,
+    SocialMedia,
+    SocialMediaWrap,
+    WebsiteRights,
+    SocialLogo,
+    SocialIcons,
+    SocialIconLink
+
+} from './FooterElements'
+
+import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from 'react-icons/fa'
 
 function Footer() {
     return (
@@ -7,17 +25,52 @@ function Footer() {
                 <FooterLinksContainer>
                     <FooterLinksWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle>
-                                <FooterLink to="/signin">How it works</FooterLink>
-                                <FooterLink to="/signin">Testimonials</FooterLink>
-                                <FooterLink to="/signin">Careers</FooterLink>
-                                <FooterLink to="/signin">Investors</FooterLink>
-                                <FooterLink to="/signin">Terms of Services</FooterLink>
-
-                            </FooterLinkTitle>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>About Us</FooterLinkTitle>
+                            <FooterLink to="/signin">Testimonials</FooterLink>
+                            <FooterLink to="/signin">Careers</FooterLink>
+                            <FooterLink to="/signin">Investors</FooterLink>
+                            <FooterLink to="/signin">Terms of Services</FooterLink>
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to='/'>
+                            crypto
+                        </SocialLogo>
+                        <WebsiteRights>crypto @ {new Date().getFullYear()} made by bieldiaz</WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank" arial-label="Facebook"><FaFacebook /></SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" arial-label="Instagram"><FaInstagram /></SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" arial-label="Youtube"><FaYoutube /></SocialIconLink>
+                            <SocialIconLink href="/" target="_blank" arial-label="Linkedin"><FaLinkedin /></SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
+
             </FooterWrap>
         </FooterContainer>
     )
